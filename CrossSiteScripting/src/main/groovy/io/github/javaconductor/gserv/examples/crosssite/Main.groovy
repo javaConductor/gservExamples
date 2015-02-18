9950e9790d9d77a20e33b02b6f4e937666ebc7fd
javaConductor <javaConductor@yahoo.com>
2015-02-18 01:12:10 -0600

    bug fixes

--- CrossSiteScripting/src/main/groovy/io/github/javaconductor/gserv/examples/crosssite/Main.groovy
+++ CrossSiteScripting/src/main/groovy/io/github/javaconductor/gserv/examples/crosssite/Main.groovy
@@ -1,7 +1,6 @@
 package io.github.javaconductor.gserv.examples.crosssite
 
 import io.github.javaconductor.gserv.GServ
-import io.github.javaconductor.gserv.plugins.cors.CORSMode
 
 class Main {
 
@@ -22,31 +21,38 @@ class Main {
           maxAge              : 7200,
           customRequestHeaders: ['X-Custom-Header']
         ]
-    ];
+    ]
+
+    gserv.plugins{
+      plugin ( 'cors', [:] )
+    }.http{
+      useResourceDocs(true)
+      get('/index.html',file("text/html", "crosssite/index.html"))
+
+    }.start(60000);
 
     gserv.plugins{
       plugin ( 'cors', [:] )
     }.http{
       cors('/public', allowAll(3600))
-      cors('/private', whiteList(3600, hostList))
+      cors('/private/*', whiteList(3600, hostList))
       cors('/internal', whiteList(3600, hostListWithLocalhost))
 
       useResourceDocs(true)
 
-      get('/index.html',file("text/html", "crosssite/index.html"))
       get('/public'){ ->
-        writeJson(msg: "This is the public message.")
+        write( "This is the public message.")
       }
 
       get('/private'){ ->
-        writeJson(msg: "This is the private message.")
+        write( "This is the private message.")
       }
 
       get('/internal'){ ->
-        writeJson(msg: "This is the internal message.")
+        write( "This is the internal message.")
       }
 
-    }.start(60000);
+    }.start(60001);
   };
 
 }
--- ETag/src/main/groovy/io/github/javaconductor/gserv/samples/etag/Main.groovy
+++ ETag/src/main/groovy/io/github/javaconductor/gserv/samples/etag/Main.groovy
@@ -9,7 +9,6 @@ class Main {
 
   static public void main(String[] args) {
     println "Main started!!"
-    def pluginMgr = PluginMgr.instance()
 
     def asMap = { thing ->
       thing.class.declaredFields { !it.synthetic }.collectEntries {
@@ -23,12 +22,41 @@ class Main {
       plugin("caching", [:])
       plugin("compression", [:])
     }.http {
-      useResourceDocs(true)
+      useResourceDocs( true )
+
       get("/", file("text/plain", "data/rfile.txt"))
+
+      // This will create a Base64 String for the eTag
       strongETag( "/*"){exch, data ->
         //MD5 it
         Encoder.md5WithBase64(data)
       }
-    }.start(54321)
+
+      get('/answers/:op/:num1:Number/:num2:Number'){ op, num1, num2 ->
+        def ans
+        switch(op){
+          case 'add': ans = num1 + num2; break;
+          case 'multiply':  ans = (num1 ) * (num2 ); break;
+          default: ans = Double.NaN;
+        }
+
+        writeJson(ans : ans)
+      }
+
+//    https://github.com/javaConductor/gserv/issues/11
+//      THIS DOES NOT WORK - ONLY Context is passed.
+//      // This will create an eTag based sole on the request values
+//      // this is fine as long as a set of results always returns the same value.
+//      /// Example: when a GET is sent tot this resource like: /add/23.12/43.45, the
+//      /// response will always be 66.57
+//      weakETag( "/answers/:op/:num1/:num2"){op, num1, num2 ->
+//        Encoder.md5WithBase64("$num1_$op_$num2".bytes)
+//      }
+
+/*      weakETag( "/answers/:op/:num1/:num2"){ context ->
+        Encoder.md5WithBase64(context.requestURI.path.bytes)
+      }
+*/
+      }.start(54321)
   }//main
 }
